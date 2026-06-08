ccec71bf9c8f4dec42e32780bccd711f17f5d265
v-kamerdinerov <anakin174jedi@gmail.com>
2026-06-08 12:41:42 +0500

    chore: bump version to 1.4.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         minSdk = 21
         targetSdk = 36
         versionCode = 7
-        versionName = "1.3.1"
+        versionName = "1.4.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
